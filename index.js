bc9f06f87d07e64927d5c89b36111b8e144f110b
yasmikash <yashtricksz@gmail.com>
2022-05-11 15:55:49 +0530

    update index file

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
-const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");
-const paymentRoute = require("./routes/payment");
 const cors = require("cors");
 
 dotenv.config();
@@ -22,14 +18,8 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/checkout", paymentRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
-
-// UPDATE5
